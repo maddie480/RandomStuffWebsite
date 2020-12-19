66a2edf49a3c8e07aa71cefc780d5f4f7c896bd7
max4805 <max480@free.fr>
2020-12-19 18:23:12 +0100

    Update Checker: set up 5-minute cache to reduce server load + use a Cloud Storage backup instead of a backup server

--- src/main/java/com/max480/randomstuff/gae/CelesteModUpdateService.java
+++ src/main/java/com/max480/randomstuff/gae/CelesteModUpdateService.java
@@ -1,5 +1,6 @@
 package com.max480.randomstuff.gae;
 
+import com.google.cloud.storage.*;
 import org.apache.commons.io.IOUtils;
 
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.time.ZonedDateTime;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,13 +19,17 @@ public class CelesteModUpdateService extends HttpServlet {
 
     private final Logger logger = Logger.getLogger("CelesteModUpdateService");
 
+    private byte[] cachedYaml;
+    private ZonedDateTime cachedYamlValidUntil = ZonedDateTime.now().minusMonths(1);
+    private ZonedDateTime lastBackupValidUntil = ZonedDateTime.now().minusMonths(1);
+
     @Override
     public void init() {
         try {
-            logger.log(Level.INFO, "Warmup: Main server everest_update.yaml is " +
-                    IOUtils.toByteArray(getConnectionWithTimeouts(Constants.MAIN_SERVER_URL)).length + " bytes long");
-            logger.log(Level.INFO, "Warmup: Backup server everest_update.yaml is " +
-                    IOUtils.toByteArray(getConnectionWithTimeouts(Constants.BACKUP_SERVER_URL)).length + " bytes long");
+            logger.log(Level.INFO, "Warmup: everest_update.yaml from server is " +
+                    IOUtils.toByteArray(getConnectionWithTimeouts(Constants.UPDATE_CHECKER_SERVER_URL)).length + " bytes long");
+            logger.log(Level.INFO, "Warmup: Backed up everest_update.yaml is " +
+                    IOUtils.toByteArray(getConnectionWithTimeouts("https://storage.googleapis.com/max480-random-stuff.appspot.com/" + Constants.CLOUD_STORAGE_BACKUP_FILENAME)).length + " bytes long");
         } catch (Exception e) {
             logger.log(Level.WARNING, "Warming up failed: " + e.toString());
         }
@@ -32,13 +38,47 @@ public class CelesteModUpdateService extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setHeader("Content-Type", "text/yaml");
-        try {
-            byte[] yaml = IOUtils.toByteArray(getConnectionWithTimeouts(Constants.MAIN_SERVER_URL));
-            IOUtils.write(yaml, response.getOutputStream());
-        } catch (IOException e) {
-            logger.log(Level.WARNING, "Main server unreachable, falling back to backup server: " + e.toString());
-            byte[] yaml = IOUtils.toByteArray(getConnectionWithTimeouts(Constants.BACKUP_SERVER_URL));
-            IOUtils.write(yaml, response.getOutputStream());
+
+        if (cachedYamlValidUntil.isAfter(ZonedDateTime.now())) {
+            // last request to server was < 5 minutes ago, just return the same response.
+            IOUtils.write(cachedYaml, response.getOutputStream());
+        } else {
+            logger.fine("Cached yaml expired, contacting server");
+
+            try {
+                // try contacting the server and send the response to the caller.
+                byte[] yaml = IOUtils.toByteArray(getConnectionWithTimeouts(Constants.UPDATE_CHECKER_SERVER_URL));
+                IOUtils.write(yaml, response.getOutputStream());
+
+                // cache the response for the next 5 minutes.
+                cachedYaml = yaml;
+                cachedYamlValidUntil = ZonedDateTime.now().plusMinutes(5);
+
+                if (lastBackupValidUntil.isBefore(ZonedDateTime.now())) {
+                    // the last time we updated the backup on Cloud Storage was more than an hour ago, so update it again.
+                    logger.fine("Cloud Storage backup expired, refreshing it");
+
+                    // we want to make the backup next time the minute hits 50 for... reasons
+                    lastBackupValidUntil = ZonedDateTime.now().withMinute(50);
+                    if (lastBackupValidUntil.isBefore(ZonedDateTime.now())) {
+                        lastBackupValidUntil = lastBackupValidUntil.plusHours(1);
+                    }
+
+                    // do it asynchronously so that it doesn't slow down the response.
+                    new Thread(() -> {
+                        Storage storage = StorageOptions.newBuilder().setProjectId("max480-random-stuff").build().getService();
+                        BlobId blobId = BlobId.of("max480-random-stuff.appspot.com", Constants.CLOUD_STORAGE_BACKUP_FILENAME);
+                        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("text/yaml").build();
+                        storage.create(blobInfo, yaml);
+                        storage.createAcl(blobId, Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER));
+                    }).start();
+                }
+            } catch (IOException e) {
+                // if server doesn't respond, we should send the Google Cloud Storage backup instead.
+                logger.log(Level.WARNING, "Server unreachable, falling back to backup: " + e.toString());
+                byte[] yaml = IOUtils.toByteArray(getConnectionWithTimeouts("https://storage.googleapis.com/max480-random-stuff.appspot.com/" + Constants.CLOUD_STORAGE_BACKUP_FILENAME));
+                IOUtils.write(yaml, response.getOutputStream());
+            }
         }
     }
 
--- src/main/java/com/max480/randomstuff/gae/ConstantsDummy.java
+++ src/main/java/com/max480/randomstuff/gae/ConstantsDummy.java
@@ -10,6 +10,6 @@ public final class ConstantsDummy {
     public static final String MAX480_WARNING_WEBHOOK = "";
     public static final String COLOURSOFNOISE_WARNING_WEBHOOK = "";
 
-    public static final String MAIN_SERVER_URL = "";
-    public static final String BACKUP_SERVER_URL = "";
+    public static final String UPDATE_CHECKER_SERVER_URL = "";
+    public static final String CLOUD_STORAGE_BACKUP_FILENAME = "";
 }
