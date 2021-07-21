46c2b79bee6bda9e69b02cc78356c33656bf7bc1
max4805 <max480@free.fr>
2021-07-21 13:31:41 +0200

    Mod update service: refresh from backend on API call instead of every 5 minutes
    The backend will call frontend APIs after the database refresh, in order to react more quickly to updates.

--- src/main/java/com/max480/randomstuff/gae/CelesteModUpdateService.java
+++ src/main/java/com/max480/randomstuff/gae/CelesteModUpdateService.java
@@ -11,71 +11,62 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.time.ZonedDateTime;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@WebServlet(name = "CelesteModUpdateService", urlPatterns = {"/celeste/everest_update.yaml", "/celeste/file_ids.yaml"})
+@WebServlet(name = "CelesteModUpdateService", loadOnStartup = 3, urlPatterns = {"/celeste/everest_update.yaml",
+        "/celeste/file_ids.yaml", "/celeste/everest-update-reload"})
 public class CelesteModUpdateService extends HttpServlet {
 
     private final Logger logger = Logger.getLogger("CelesteModUpdateService");
 
-    private byte[] cachedYaml;
-    private ZonedDateTime cachedYamlValidUntil = ZonedDateTime.now().minusMonths(1);
-    private ZonedDateTime lastBackupValidUntil = ZonedDateTime.now().minusMonths(1);
+    private byte[] everestYaml;
+
+    @Override
+    public void init() {
+        try {
+            logger.fine("Downloading everest_update.yaml from Cloud Storage");
+            everestYaml = IOUtils.toByteArray(getConnectionWithTimeouts("https://storage.googleapis.com/max480-random-stuff.appspot.com/" + Constants.CLOUD_STORAGE_BACKUP_FILENAME));
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Warming up failed: " + e.toString());
+        }
+    }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setHeader("Content-Type", "text/yaml");
-
         if ("/celeste/file_ids.yaml".equals(request.getRequestURI())) {
+            // transfer file_ids.yaml from backend
+            response.setHeader("Content-Type", "text/yaml");
             try (InputStream is = getConnectionWithTimeouts(Constants.FILE_IDS_URL).getInputStream()) {
                 IOUtils.copy(is, response.getOutputStream());
             }
-            return;
-        }
-
-        if (cachedYamlValidUntil.isAfter(ZonedDateTime.now())) {
-            // last request to server was < 5 minutes ago, just return the same response.
-            IOUtils.write(cachedYaml, response.getOutputStream());
+        } else if (request.getRequestURI().equals("/celeste/everest-update-reload")
+                && ("key=" + Constants.CATALOG_RELOAD_SHARED_SECRET).equals(request.getQueryString())) {
+            // trigger a reload of everest_update.yaml
+            reloadEverestUpdate();
+        } else if (request.getRequestURI().equals("/celeste/everest_update.yaml")) {
+            // send the everest_update.yaml we have in cache
+            response.setHeader("Content-Type", "text/yaml");
+            IOUtils.write(everestYaml, response.getOutputStream());
         } else {
-            logger.fine("Cached yaml expired, contacting server");
-
-            try {
-                // try contacting the server and send the response to the caller.
-                byte[] yaml = IOUtils.toByteArray(getConnectionWithTimeouts(Constants.UPDATE_CHECKER_SERVER_URL));
-                IOUtils.write(yaml, response.getOutputStream());
-
-                // cache the response for the next 5 minutes.
-                cachedYaml = yaml;
-                cachedYamlValidUntil = ZonedDateTime.now().plusMinutes(5);
-
-                if (lastBackupValidUntil.isBefore(ZonedDateTime.now())) {
-                    // the last time we updated the backup on Cloud Storage was more than an hour ago, so update it again.
-                    logger.fine("Cloud Storage backup expired, refreshing it");
-
-                    // we want to make the backup next time the minute hits 50 for... reasons
-                    lastBackupValidUntil = ZonedDateTime.now().withMinute(50);
-                    if (lastBackupValidUntil.isBefore(ZonedDateTime.now())) {
-                        lastBackupValidUntil = lastBackupValidUntil.plusHours(1);
-                    }
-
-                    // do it asynchronously so that it doesn't slow down the response.
-                    new Thread(() -> {
-                        Storage storage = StorageOptions.newBuilder().setProjectId("max480-random-stuff").build().getService();
-                        BlobId blobId = BlobId.of("max480-random-stuff.appspot.com", Constants.CLOUD_STORAGE_BACKUP_FILENAME);
-                        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("text/yaml").build();
-                        storage.create(blobInfo, yaml);
-                        storage.createAcl(blobId, Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER));
-                    }).start();
-                }
-            } catch (IOException e) {
-                // if server doesn't respond, we should send the Google Cloud Storage backup instead.
-                logger.log(Level.WARNING, "Server unreachable, falling back to backup: " + e.toString());
-                byte[] yaml = IOUtils.toByteArray(getConnectionWithTimeouts("https://storage.googleapis.com/max480-random-stuff.appspot.com/" + Constants.CLOUD_STORAGE_BACKUP_FILENAME));
-                IOUtils.write(yaml, response.getOutputStream());
-            }
+            response.setStatus(404);
         }
+    }
+
+    private void reloadEverestUpdate() throws IOException {
+        logger.fine("Refreshing everest_update.yaml from backend server and backing up on Cloud Storage");
+
+        // try contacting the server and cache the response.
+        byte[] yaml = IOUtils.toByteArray(getConnectionWithTimeouts(Constants.UPDATE_CHECKER_SERVER_URL));
+
+        // back up on Cloud Storage.
+        Storage storage = StorageOptions.newBuilder().setProjectId("max480-random-stuff").build().getService();
+        BlobId blobId = BlobId.of("max480-random-stuff.appspot.com", Constants.CLOUD_STORAGE_BACKUP_FILENAME);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("text/yaml").build();
+        storage.create(blobInfo, yaml);
+        storage.createAcl(blobId, Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER));
+
+        everestYaml = yaml;
     }
 
     public static URLConnection getConnectionWithTimeouts(String url) throws IOException {
