a84cd33367557ec7b98b69f6ff9b76eb82c41997
max4805 <max480@free.fr>
2021-07-30 00:00:27 +0200

    Fix / -> /celeste redirect

--- src/main/java/com/max480/randomstuff/gae/ServerRootService.java
+++ src/main/java/com/max480/randomstuff/gae/ServerRootService.java
@@ -6,12 +6,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "IndexService", urlPatterns = {"/"})
-public class IndexService extends HttpServlet {
+@WebServlet(name = "ServerRootService", urlPatterns = {""})
+public class ServerRootService extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         // for now there is nothing public but Celeste stuff, so just redirect there.
         response.setStatus(302);
-        response.setHeader("Location", "/celeste");
+        response.setHeader("Location", "/celeste/");
     }
 }
