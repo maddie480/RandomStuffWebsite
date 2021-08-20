4dd7d1c5a50d95bb90a96b6e8dfd5b263f644ec4
max4805 <max480@free.fr>
2021-08-20 21:35:41 +0200

    Update ConstantsDummy accordingly to Constants

--- src/main/java/com/max480/randomstuff/gae/ConstantsDummy.java
+++ src/main/java/com/max480/randomstuff/gae/ConstantsDummy.java
@@ -7,11 +7,6 @@ public final class ConstantsDummy {
     public static final String CATALOG_RELOAD_SHARED_SECRET = "";
     public static final String SERVICE_MONITORING_SECRET = "";
 
-    public static final String UPDATE_CHECKER_SERVER_URL = "";
-    public static final String MOD_SEARCH_DATABASE_URL = "";
-    public static final String CUSTOM_ENTITY_CATALOG_URL = "";
-    public static final String FILE_IDS_URL = "";
-
     public static final String GITHUB_USERNAME = "";
     public static final String GITHUB_PERSONAL_ACCESS_TOKEN = "";
 
