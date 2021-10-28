18865dfa1263b9bbc9d7d40f48e387bc7c626e8b
max4805 <max480@free.fr>
2021-10-28 18:43:36 +0200

    Added #celeste_news_network subscription service

---
+++ src/main/java/com/max480/randomstuff/gae/CelesteNewsNetworkSubscriptionService.java
@@ -0,0 +1,154 @@
+package com.max480.randomstuff.gae;
+
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import static com.max480.randomstuff.gae.CelesteModUpdateService.getCloudStorageInputStream;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+/**
+ * Servlet allowing to subscribe to the #celeste_news_network channel from Celestecord.
+ */
+@WebServlet(name = "CelesteNewsNetworkSubscriptionService", urlPatterns = {"/celeste/news-network-subscription"})
+@MultipartConfig
+public class CelesteNewsNetworkSubscriptionService extends HttpServlet {
+    private static final Logger logger = Logger.getLogger("CelesteNewsNetworkSubscriptionService");
+    private static final Storage storage = StorageOptions.newBuilder().setProjectId("max480-random-stuff").build().getService();
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.setAttribute("bad_request", false);
+        request.setAttribute("bad_webhook", false);
+        request.setAttribute("not_registered", false);
+        request.setAttribute("already_registered", false);
+        request.setAttribute("subscribe_success", false);
+        request.setAttribute("unsubscribe_success", false);
+        request.getRequestDispatcher("/WEB-INF/celeste-news-network-subscription.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("bad_request", false);
+        request.setAttribute("bad_webhook", false);
+        request.setAttribute("not_registered", false);
+        request.setAttribute("already_registered", false);
+        request.setAttribute("subscribe_success", false);
+        request.setAttribute("unsubscribe_success", false);
+
+        String webhook = request.getParameter("url");
+        String action = request.getParameter("action");
+
+        if (webhook == null || action == null) {
+            logger.warning("Missing parameter!");
+            request.setAttribute("bad_request", true);
+
+        } else if (!webhook.matches("^https://discord\\.com/api/webhooks/[0-9]+/[A-Za-z0-9-_]+$")) {
+            logger.warning("Webhook does not follow regex: " + webhook);
+            request.setAttribute("bad_webhook", true);
+
+        } else {
+            List<String> webhookUrls;
+            try (InputStream is = getCloudStorageInputStream("celeste_news_network_subscribers.json")) {
+                webhookUrls = new JSONArray(IOUtils.toString(is, UTF_8)).toList()
+                        .stream()
+                        .map(Object::toString)
+                        .collect(Collectors.toCollection(ArrayList::new));
+            }
+
+            boolean save = false;
+
+            if (action.equals("Subscribe")) {
+                if (webhookUrls.contains(webhook)) {
+                    logger.warning("Webhook already registered: " + webhook);
+                    request.setAttribute("already_registered", true);
+
+                } else if (isWebhookWorking(webhook)) {
+                    logger.info("New webhook registered: " + webhook);
+                    webhookUrls.add(webhook);
+                    save = true;
+                    request.setAttribute("subscribe_success", true);
+
+                } else {
+                    logger.warning("Webhook does not work: " + webhook);
+                    request.setAttribute("bad_webhook", true);
+                }
+            } else {
+                if (webhookUrls.contains(webhook)) {
+                    logger.info("Webhook unregistered: " + webhook);
+                    webhookUrls.remove(webhook);
+                    save = true;
+                    request.setAttribute("unsubscribe_success", true);
+
+                } else {
+                    logger.warning("Webhook is not registered: " + webhook);
+                    request.setAttribute("not_registered", true);
+                }
+            }
+
+            if (save) {
+                BlobId blobId = BlobId.of("max480-random-stuff.appspot.com", "celeste_news_network_subscribers.json");
+                BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
+                        .setContentType("application/json")
+                        .setCacheControl("no-store")
+                        .build();
+                storage.create(blobInfo, new JSONArray(webhookUrls).toString().getBytes(UTF_8));
+            }
+        }
+
+        request.getRequestDispatcher("/WEB-INF/celeste-news-network-subscription.jsp").forward(request, response);
+    }
+
+    /**
+     * Checks if the webhook is working, by sending "This webhook was registered on the #celeste_news_network subscription service!"
+     * to it. This doubles as a notification in the server.
+     *
+     * @param webhookUrl The URL of the webhook to test
+     * @return Whether the call succeeded or not
+     */
+    private boolean isWebhookWorking(String webhookUrl) {
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(webhookUrl).openConnection();
+
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0");
+
+            connection.connect();
+
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+            writer.write("{\"avatar_url\": \"https://cdn.discordapp.com/embed/avatars/0.png\", \"username\": \"Subscription Service\"," +
+                    " \"content\": \"This webhook was registered on the #celeste_news_network subscription service!\"}");
+            writer.close();
+
+            return connection.getResponseCode() == 204 || connection.getResponseCode() == 200;
+        } catch (IOException e) {
+            // well, the webhook is definitely not working. :p
+            logger.severe("I/O error while calling webhook: " + e);
+            return false;
+        }
+    }
+}
---
+++ src/main/webapp/WEB-INF/celeste-news-network-subscription.jsp
@@ -0,0 +1,100 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<%@ page import="java.util.List, java.util.Map, com.max480.randomstuff.gae.CelesteModCatalogService, static org.apache.commons.text.StringEscapeUtils.escapeHtml4"%>
+
+<%@page session="false"%>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <title>#celeste_news_network Subscription Service</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="max480">
+    <meta name="description" content="If you want the #celeste_news_network channel from Mt. Celeste Climbing Association on your server, register your Discord webhook URL on this page!">
+    <meta property="og:title" content="#celeste_news_network Subscription Service">
+    <meta property="og:description" content="If you want the #celeste_news_network channel from Mt. Celeste Climbing Association on your server, register your Discord webhook URL on this page!">
+
+    <link rel="shortcut icon" href="/celeste/favicon.ico">
+
+    <link rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+        crossorigin="anonymous">
+
+    <link rel="stylesheet" href="/css/common-v7.css">
+</head>
+
+<body>
+    <div class="container">
+        <div id="nav">
+            <a href="/celeste/custom-entity-catalog">Custom&nbsp;Entity&nbsp;Catalog</a> <span class="sep">|</span>
+            <a href="/celeste/everest-yaml-validator">everest.yaml&nbsp;validator</a> <span class="sep">|</span>
+            <a href="/celeste/update-checker-status">Update&nbsp;Checker&nbsp;status</a> <span class="sep">|</span>
+            <a href="https://max480-random-stuff.herokuapp.com/banana-mirror-browser">Banana&nbsp;Mirror&nbsp;Browser</a> <span class="sep break">|</span>
+            <a href="/celeste/font-generator">Font&nbsp;Generator</a> <span class="sep">|</span>
+            <a href="https://max480-random-stuff.herokuapp.com/wipe-converter">Wipe&nbsp;Converter</a> <span class="sep">|</span>
+            <a href="/discord-bots.html">Discord&nbsp;Bots</a>
+        </div>
+
+        <h1>#celeste_news_network subscription service</h1>
+
+        <% if((boolean) request.getAttribute("bad_request")) { %>
+            <div class="alert alert-danger">
+                Your request was invalid, please try again.
+            </div>
+        <% } else if((boolean) request.getAttribute("bad_webhook")) { %>
+            <div class="alert alert-danger">
+                Your Discord webhook link is invalid.
+            </div>
+        <% } else if((boolean) request.getAttribute("not_registered")) { %>
+            <div class="alert alert-warning">
+                Your webhook is not registered! You cannot unregister it.
+            </div>
+        <% } else if((boolean) request.getAttribute("already_registered")) { %>
+            <div class="alert alert-info">
+                Your webhook is already registered!
+            </div>
+        <% } else if((boolean) request.getAttribute("subscribe_success")) { %>
+            <div class="alert alert-success">
+                Your webhook was registered successfully! You should have received the test message on it.
+            </div>
+        <% } else if((boolean) request.getAttribute("unsubscribe_success")) { %>
+            <div class="alert alert-success">
+                Your webhook was unregistered successfully.
+            </div>
+        <% } %>
+
+        <p>
+            This page allows you to register a Discord webhook in order to have a copy of the <code>#celeste_news_network</code> channel from
+            the <a href="https://discord.gg/celeste" target="_blank">Mt. Celeste Climbing Association</a> on your server.
+            This webhook will receive all new tweets from <a href="https://twitter.com/celeste_game" target="_blank">@celeste_game</a>
+            and <a href="https://twitter.com/EverestAPI" target="_blank">@EverestAPI</a>.
+        </p>
+
+        <div class="alert alert-info">
+            When registering your webhook, a message saying <i>"This webhook was registered on the #celeste_news_network subscription service!"</i> will be sent to it.
+        </div>
+
+        <p>
+            You can unsubscribe by deleting the webhook, or by returning to this page and clicking "Unsubscribe".
+        </p>
+
+        <p>
+            To sign up, just create a webhook on your server pointing to the channel of your choice, and paste it here:
+        </p>
+
+        <form method="POST">
+            <div class="form-group">
+                <label for="url"><b>Discord Webhook URL</b></label>
+                <input type="text" class="form-control" id="url" name="url" required pattern="^https:\/\/discord\.com\/api\/webhooks\/[0-9]+/[A-Za-z0-9-_]+$">
+            </div>
+
+            <input type="submit" class="btn btn-success" name="action" value="Subscribe">
+            <input type="submit" class="btn btn-danger" name="action" value="Unsubscribe">
+            <br/>
+        </form>
+    </div>
+</body>
+</html>
