ea1c7a546d94e9a48dbb6f5c9bf490717aa328f5
max4805 <max480@free.fr>
2021-11-11 11:54:02 +0100

    Move some APIs that had no reason to be APIs to the backend
    ... when an API is protected by a key, isn't used by any page or API on the website, and is only called daily by the backend... then that probably means it should be part of the backend.

--- src/main/java/com/max480/randomstuff/gae/SecretConstants.java
+++ src/main/java/com/max480/randomstuff/gae/SecretConstants.java
@@ -17,7 +17,6 @@ public class SecretConstants {
     private static final Logger logger = Logger.getLogger("SecretConstants");
 
     public static String CATALOG_RELOAD_SHARED_SECRET = "";
-    public static String SERVICE_MONITORING_SECRET = "";
 
     public static String SRC_MOD_LIST_KEY = "";
 
@@ -41,7 +40,6 @@ public class SecretConstants {
     static {
         Map<String, Consumer<String>> secrets = new HashMap<>();
         secrets.put("CATALOG_RELOAD_SHARED_SECRET", s -> CATALOG_RELOAD_SHARED_SECRET = s);
-        secrets.put("SERVICE_MONITORING_SECRET", s -> SERVICE_MONITORING_SECRET = s);
         secrets.put("SRC_MOD_LIST_KEY", s -> SRC_MOD_LIST_KEY = s);
         secrets.put("GITHUB_USERNAME", s -> GITHUB_USERNAME = s);
         secrets.put("GITHUB_PERSONAL_ACCESS_TOKEN", s -> GITHUB_PERSONAL_ACCESS_TOKEN = s);
