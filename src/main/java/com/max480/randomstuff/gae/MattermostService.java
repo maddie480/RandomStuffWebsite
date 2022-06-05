295ea7c9485e6a42827575b2ace7db48eea97cf7
max4805 <max480@free.fr>
2022-06-05 10:18:51 +0200

    Add serialVersionUID to PlanningExploit
    This thing will eventually come from the backend pre-parsed, as the parsing takes more and more time and slows down service startup.

--- src/main/java/com/max480/randomstuff/gae/MattermostService.java
+++ src/main/java/com/max480/randomstuff/gae/MattermostService.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -59,7 +60,9 @@ public class MattermostService extends HttpServlet {
         }
     }
 
-    private static class PlanningExploit {
+    private static class PlanningExploit implements Serializable {
+        private static final long serialVersionUID = 56185131613831863L;
+
         private final List<ZonedDateTime> exploitTimes = new ArrayList<>();
         private final List<String> principalExploit = new ArrayList<>();
         private final List<String> backupExploit = new ArrayList<>();
