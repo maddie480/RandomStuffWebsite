d627413477ee467693ca2688baa704e0c092b788
max4805 <max480@free.fr>
2022-09-03 22:03:25 +0200

    Add embeds to Wipe Converter and Banana Mirror Browser pages

--- src/main/java/com/max480/randomstuff/gae/VueServer.java
+++ src/main/java/com/max480/randomstuff/gae/VueServer.java
@@ -1,6 +1,7 @@
 package com.max480.randomstuff.gae;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.text.StringEscapeUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * This servlet is just here to serve the Vue app.
@@ -20,7 +22,23 @@ public class VueServer extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         try (InputStream is = VueServer.class.getClassLoader().getResourceAsStream("vue-index.html")) {
-            IOUtils.copy(is, response.getOutputStream());
+            String contents = IOUtils.toString(is, StandardCharsets.UTF_8);
+
+            if (request.getRequestURI().equals("/celeste/banana-mirror-browser")) {
+                contents = contents
+                        .replace("${favicon}", StringEscapeUtils.escapeHtml4("/celeste/favicon.ico"))
+                        .replace("${page_title}", StringEscapeUtils.escapeHtml4("Banana Mirror Browser"))
+                        .replace("${page_description}", StringEscapeUtils.escapeHtml4(
+                                "If GameBanana is down or being slow, you can download Celeste mods here instead."));
+            } else {
+                contents = contents
+                        .replace("${favicon}", StringEscapeUtils.escapeHtml4("/celeste/favicon.ico"))
+                        .replace("${page_title}", StringEscapeUtils.escapeHtml4("Wipe Converter"))
+                        .replace("${page_description}", StringEscapeUtils.escapeHtml4(
+                                "Upload the frames of a custom Celeste screen wipe here, and you will be able to use it in-game with the \"max480's Helping Hand\" mod."));
+            }
+
+            response.getWriter().write(contents);
         }
     }
 }
