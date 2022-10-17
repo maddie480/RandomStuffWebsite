e74e129db17446079cd919c6c96d31b09272113b
max4805 <52103563+max4805@users.noreply.github.com>
2022-10-17 16:05:59 +0200

    Show latest update (full or incremental) on status page

--- src/main/java/com/max480/randomstuff/gae/UpdateCheckerStatusService.java
+++ src/main/java/com/max480/randomstuff/gae/UpdateCheckerStatusService.java
@@ -54,7 +54,6 @@ public class UpdateCheckerStatusService extends HttpServlet {
         // the service is down until proven otherwise.
         request.setAttribute("up", false);
 
-        long lastFullCheckTimestamp;
         long lastCheckTimestamp;
         int lastCheckDuration;
         List<LatestUpdatesEntry> latestUpdatesEntries;
@@ -64,7 +63,6 @@ public class UpdateCheckerStatusService extends HttpServlet {
         try (InputStream is = CloudStorageUtils.getCloudStorageInputStream("update_checker_status.json")) {
             updaterStatusJson = new JSONObject(IOUtils.toString(is, StandardCharsets.UTF_8));
 
-            lastFullCheckTimestamp = updaterStatusJson.getLong("lastFullCheckTimestamp");
             lastCheckTimestamp = Math.max(updaterStatusJson.getLong("lastFullCheckTimestamp"),
                     updaterStatusJson.getLong("lastIncrementalCheckTimestamp"));
             lastCheckDuration = updaterStatusJson.getInt("lastCheckDuration");
@@ -76,11 +74,10 @@ public class UpdateCheckerStatusService extends HttpServlet {
             request.setAttribute("modCount", updaterStatusJson.getInt("modCount"));
         }
 
-        if (lastFullCheckTimestamp > 0) {
-            ZonedDateTime timeUtc = Instant.ofEpochMilli(lastFullCheckTimestamp).atZone(ZoneId.of("UTC"));
-            ZonedDateTime timeUtcLast = Instant.ofEpochMilli(lastCheckTimestamp).atZone(ZoneId.of("UTC"));
+        if (lastCheckTimestamp > 0) {
+            ZonedDateTime timeUtc = Instant.ofEpochMilli(lastCheckTimestamp).atZone(ZoneId.of("UTC"));
 
-            if (timeUtcLast.isAfter(ZonedDateTime.now().minusMinutes(30))) {
+            if (timeUtc.isAfter(ZonedDateTime.now().minusMinutes(30))) {
                 // consider the service to be up if the latest successful update was less than 30 minutes ago.
                 request.setAttribute("up", true);
             }
