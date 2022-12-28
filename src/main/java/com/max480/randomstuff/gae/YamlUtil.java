6684bb85f96285bf509e046188082bed248a8c51
max4805 <52103563+max4805@users.noreply.github.com>
2022-12-28 13:04:47 +0100

    Sync YamlUtil with backend

--- src/main/java/com/max480/randomstuff/gae/YamlUtil.java
+++ src/main/java/com/max480/randomstuff/gae/YamlUtil.java
@@ -1,6 +1,5 @@
 package com.max480.randomstuff.gae;
 
-import org.apache.commons.io.IOUtils;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
@@ -14,7 +13,21 @@ import java.nio.charset.StandardCharsets;
  * A utility class to parse and dump YAML objects.
  */
 public class YamlUtil {
-    private static final Yaml yaml = new Yaml(new SafeConstructor(new LoaderOptions()), new Representer(new DumperOptions()));
+    private static final Yaml yaml;
+
+    static {
+        // mod_search_database.yaml is larger than 3 MB, which is the default code point limit in SnakeYAML.
+        // So we need to raise it a bit! We set it to 8 MB instead.
+        LoaderOptions loaderOptions = new LoaderOptions();
+        loaderOptions.setCodePointLimit(8 * 1024 * 1024);
+
+        // use the block flow style rather than the default "block at the root level, flow on deeper levels" default.
+        DumperOptions dumperOptions = new DumperOptions();
+        dumperOptions.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+
+        // use SafeConstructor to avoid callers being able to construct arbitrary Java objects
+        yaml = new Yaml(new SafeConstructor(loaderOptions), new Representer(dumperOptions), dumperOptions, loaderOptions);
+    }
 
     /**
      * Loads YAML data from an input stream.
@@ -28,7 +41,7 @@ public class YamlUtil {
      */
     public static void dump(Object data, OutputStream os) throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, StandardCharsets.UTF_8))) {
-            IOUtils.write(yaml.dumpAs(data, null, DumperOptions.FlowStyle.BLOCK), writer);
+            yaml.dump(data, writer);
         }
     }
 }
