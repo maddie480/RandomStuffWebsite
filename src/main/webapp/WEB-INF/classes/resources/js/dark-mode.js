e0aa1840d38a92e758c1968e6f9de4dadf46f5d0
maddie480 <52103563+maddie480@users.noreply.github.com>
2023-06-04 20:23:58 +0200

    Handle outline-dark/light buttons when toggling themes

--- src/main/webapp/WEB-INF/classes/resources/js/dark-mode.js
+++ src/main/webapp/WEB-INF/classes/resources/js/dark-mode.js
@@ -2,8 +2,10 @@
     const updateTheme = function() {
         if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
             document.documentElement.setAttribute('data-bs-theme', 'dark');
+            document.querySelectorAll('.btn-outline-dark').forEach(e => e.classList.replace('btn-outline-dark', 'btn-outline-light'));
         } else {
             document.documentElement.setAttribute('data-bs-theme', 'light');
+            document.querySelectorAll('.btn-outline-light').forEach(e => e.classList.replace('btn-outline-light', 'btn-outline-dark'));
         }
     }
 
