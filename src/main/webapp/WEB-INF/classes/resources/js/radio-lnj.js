19ed9c85209e29cdc2ebb5520e4ae86459a5880a
Maddie <52103563+maddie480@users.noreply.github.com>
2023-09-05 18:55:26 +0200

    g pa testé

--- src/main/webapp/WEB-INF/classes/resources/js/radio-lnj.js
+++ src/main/webapp/WEB-INF/classes/resources/js/radio-lnj.js
@@ -1,81 +1,118 @@
 {
     const audio = new Audio();
-    let playlist = [];
-    let updateTimerTimeoutHandle;
-    let nextSongTimeoutHandle;
 
-    const getTrackName = () => {
-        const length = (playlist[0].duration - 5000 ) / 1000;
-        let minutes = Math.floor(length / 60);
-        let seconds = Math.floor(length % 60);
+    const formatTime = (totalSeconds, separator) => {
+        let minutes = Math.floor(totalSeconds / 60);
+        let seconds = Math.floor(totalSeconds % 60);
 
+        // eh, might as well abuse the "JS is not typed" thingy
         if (minutes < 10) minutes = '0' + minutes;
         if (seconds < 10) seconds = '0' + seconds;
 
-        return playlist[0].trackName + ' (' + minutes + ':' + seconds + ')';
-    }
+        return minutes + separator + seconds;
+    };
 
-    const updateTimer = () => {
-        if (audio.paused) {
-            document.getElementById('timer').innerText = '-- --';
-            updateTimerTimeoutHandle = setTimeout(updateTimer, 1000);
-        } else {
+    {
+        // === music progress timer
+
+        let updateTimerTimeoutHandle;
+
+        const updateTimer = () => {
             const progress = audio.currentTime;
-            let minutes = Math.floor(progress / 60);
-            let seconds = Math.floor(progress % 60);
-
-            // eh, might as well abuse the "JS is not typed" thingy
-            if (minutes < 10) minutes = '0' + minutes;
-            if (seconds < 10) seconds = '0' + seconds;
-
-            document.getElementById('timer').innerText = minutes + ' ' + seconds;
+            document.getElementById('timer').innerText = formatTime(progress, ' ');
 
             const millisUntilNextSecond = 1000 - ((progress - Math.floor(progress)) * 1000);
             updateTimerTimeoutHandle = setTimeout(updateTimer, millisUntilNextSecond);
-        }
-    };
+        };
 
-    const nextSong = () => {
-        const doneElement = playlist.splice(0, 1);
-        playlist.push(doneElement);
+        audio.addEventListener('play', updateTimer);
 
-        audio.src = playlist[0].path;
-        audio.currentTime = 0;
-        document.getElementById('title').innerText = getTrackName();
-        audio.play();
-        nextSongTimeoutHandle = setTimeout(nextSong, playlist[0].duration);
-    };
+        audio.addEventListener('pause', () => {
+            clearTimeout(updateTimerTimeoutHandle);
 
-    const startPlaying = async function() {
-        const playlistResource = await (await fetch('/radio-lnj/playlist.json')).json();
-        playlist = playlistResource.playlist;
+            document.getElementById('timer').innerText = '-- --';
+            document.getElementById('title').innerText = '--';
+        });
+    }
 
-        audio.src = playlist[0].path;
-        audio.currentTime = playlistResource.seek / 1000.;
-        document.getElementById('title').innerText = getTrackName();
-        audio.play();
+    {
+        // === playlist handling
 
-        nextSongTimeoutHandle = setTimeout(nextSong, playlist[0].duration - playlistResource.seek);
-        updateTimer();
+        let playlist = [];
+        let nextSongTimeoutHandle;
+        let playInitiatedByCode = false;
 
-        document.getElementById('play').removeEventListener('click', startPlaying);
-        document.getElementById('play').addEventListener('click', stopPlaying);
-        document.getElementById('play').innerText = 'Stop';
-    };
+        const getTrackName = () => {
+            const length = (playlist[0].duration - 5000) / 1000;
+            return playlist[0].trackName + ' (' + formatTime(length, ':') + ')';
+        };
 
-    const stopPlaying = () => {
-        audio.pause();
+        const stopPlaying = () => {
+            // actually stop playing the music
+            audio.pause();
 
-        clearTimeout(updateTimerTimeoutHandle);
-        clearTimeout(nextSongTimeoutHandle);
+            // give up on switching to the next song
+            clearTimeout(nextSongTimeoutHandle);
 
-        document.getElementById('timer').innerText = '-- --';
-        document.getElementById('title').innerText = '--';
+            // change "Stop" to "Play"
+            document.getElementById('play').removeEventListener('click', stopPlaying);
+            document.getElementById('play').addEventListener('click', startPlaying);
+            document.getElementById('play').innerText = 'Play';
+        };
 
-        document.getElementById('play').removeEventListener('click', stopPlaying);
+        const playHeadOfPlaylist = (seekTo) => {
+            audio.src = playlist[0].path;
+            audio.currentTime = seekTo;
+            document.getElementById('title').innerText = getTrackName();
+            playInitiatedByCode = true;
+
+            audio.play().catch(e => {
+                // display the error to the user and stop the radio
+                console.error(e);
+                document.getElementById('title').innerText = 'Error starting to play radio: ' + e.message;
+                stopPlaying();
+            });
+        };
+
+        const nextSong = () => {
+            // move the finished element to the end of the playlist
+            const doneElement = playlist.splice(0, 1);
+            playlist.push(doneElement);
+
+            // start playing the next element
+            playHeadOfPlaylist(0);
+
+            // prepare switching to the next song
+            nextSongTimeoutHandle = setTimeout(nextSong, playlist[0].duration);
+        };
+
+        const startPlaying = async function() {
+            // load the current progress of the playlist
+            const playlistResource = await (await fetch('/radio-lnj/playlist.json')).json();
+            playlist = playlistResource.playlist;
+
+            // play the current element
+            playHeadOfPlaylist(playlistResource.seek / 1000.);
+
+            // prepare switching to the next song
+            nextSongTimeoutHandle = setTimeout(nextSong, playlist[0].duration - playlistResource.seek);
+
+            // change "Play" to "Stop"
+            document.getElementById('play').removeEventListener('click', startPlaying);
+            document.getElementById('play').addEventListener('click', stopPlaying);
+            document.getElementById('play').innerText = 'Stop';
+        };
+
+        audio.addEventListener('play', () => {
+            if (!playInitiatedByCode) {
+                // the user resumed playing, we should catch up with the radio
+                stopPlaying();
+                startPlaying();
+            }
+
+            playInitiatedByCode = false;
+        });
+
         document.getElementById('play').addEventListener('click', startPlaying);
-        document.getElementById('play').innerText = 'Play';
-    };
-
-    document.getElementById('play').addEventListener('click', startPlaying);
+    }
 }
