7b901237354d2033eb515b472cf3ccb3c06962bb
maddie480 <52103563+maddie480@users.noreply.github.com>
2023-11-01 23:22:35 +0100

    More efficiently look up for files

--- src/main/java/com/max480/randomstuff/gae/AssetDriveService.java
+++ src/main/java/com/max480/randomstuff/gae/AssetDriveService.java
@@ -13,19 +13,44 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
-@WebServlet(name = "AssetDriveService", urlPatterns = {"/celeste/asset-drive/list/decals", "/celeste/asset-drive/list/stylegrounds",
-        "/celeste/asset-drive/list/fgtilesets", "/celeste/asset-drive/list/bgtilesets", "/celeste/asset-drive/list/misc", "/celeste/asset-drive/files/*"})
+@WebServlet(name = "AssetDriveService", loadOnStartup = 11, urlPatterns = {"/celeste/asset-drive/reload", "/celeste/asset-drive/list/decals",
+        "/celeste/asset-drive/list/stylegrounds", "/celeste/asset-drive/list/fgtilesets", "/celeste/asset-drive/list/bgtilesets",
+        "/celeste/asset-drive/list/misc", "/celeste/asset-drive/files/*"})
 public class AssetDriveService extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(AssetDriveService.class);
 
+    private Map<String, JSONObject> fileAssetMap = Collections.emptyMap();
+
+    @Override
+    public void init() {
+        try {
+            buildAssetMap();
+        } catch (Exception e) {
+            log.warn("Warming up failed!", e);
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getRequestURI().equals("/celeste/asset-drive/reload")) {
+            if (req.getQueryString() == null || !req.getQueryString().equals("key=" + SecretConstants.RELOAD_SHARED_SECRET)) {
+                // invalid secret
+                log.warn("Invalid key");
+                resp.setStatus(403);
+            } else {
+                buildAssetMap();
+            }
+            return;
+        }
+
         if (req.getRequestURI().startsWith("/celeste/asset-drive/list/")) {
             JSONObject allCategories;
             try (InputStream is = Files.newInputStream(Paths.get("/shared/temp/asset-drive/categorized-assets.json"))) {
@@ -37,22 +62,8 @@ public class AssetDriveService extends HttpServlet {
         }
 
         if (req.getRequestURI().startsWith("/celeste/asset-drive/files/")) {
-            JSONArray allFiles;
-            try (InputStream is = Files.newInputStream(Paths.get("/shared/temp/asset-drive/cached-list.json"))) {
-                allFiles = new JSONArray(IOUtils.toString(is, StandardCharsets.UTF_8));
-            }
-
             String fileId = req.getRequestURI().substring(27);
-
-            JSONObject foundFile = null;
-            for (Object o : allFiles) {
-                JSONObject file = (JSONObject) o;
-                if (fileId.equals(file.getString("id"))) {
-                    foundFile = file;
-                    break;
-                }
-            }
-
+            JSONObject foundFile = fileAssetMap.getOrDefault(fileId, null);
             log.debug("Looking for asset with id {}, found: {}", fileId, foundFile);
 
             if (foundFile != null) {
@@ -74,5 +85,23 @@ public class AssetDriveService extends HttpServlet {
         resp.setStatus(404);
         PageRenderer.render(req, resp, "page-not-found", "Page Not Found",
                 "Oops, this link seems invalid. Please try again!");
+    }
+
+    private void buildAssetMap() throws IOException {
+        JSONArray allFiles;
+        try (InputStream is = Files.newInputStream(Paths.get("/shared/temp/asset-drive/cached-list.json"))) {
+            allFiles = new JSONArray(IOUtils.toString(is, StandardCharsets.UTF_8));
+        }
+
+        Map<String, JSONObject> result = new HashMap<>();
+
+        for (Object o : allFiles) {
+            JSONObject file = (JSONObject) o;
+            result.put(file.getString("id"), file);
+        }
+
+        log.debug("Loaded asset map with {} files.", result.size());
+
+        fileAssetMap = result;
     }
 }
