a4c513cd326f28678717c71d1190098adf32842b
maddie480 <52103563+maddie480@users.noreply.github.com>
2023-11-18 19:24:32 +0100

    Remove the core branch

--- src/main/java/ovh/maddie480/randomstuff/frontend/EverestYamlValidatorService.java
+++ src/main/java/ovh/maddie480/randomstuff/frontend/EverestYamlValidatorService.java
@@ -227,7 +227,7 @@ public class EverestYamlValidatorService extends HttpServlet {
                 {
                     EverestModuleMetadata metadata = new EverestModuleMetadata();
                     metadata.Name = "EverestCore";
-                    metadata.Version = "1." + everestVersions.getInt("core") + ".0";
+                    metadata.Version = "1." + getMaximumEverestVersion(everestVersions) + ".0";
                     database.add(metadata);
                 }
                 for (EverestModuleMetadata mod : metadatas) {
@@ -478,10 +478,7 @@ public class EverestYamlValidatorService extends HttpServlet {
                         everestVersions.getInt("dev"),
                         everestVersions.getInt("beta")
                 ),
-                Math.max(
-                        everestVersions.getInt("stable"),
-                        everestVersions.getInt("core")
-                )
+                everestVersions.getInt("stable")
         );
     }
 
@@ -496,10 +493,8 @@ public class EverestYamlValidatorService extends HttpServlet {
             return everestVersions.getInt("stable");
         } else if (currentVersion <= everestVersions.getInt("beta")) {
             return everestVersions.getInt("beta");
-        } else if (currentVersion <= everestVersions.getInt("dev")) {
-            return everestVersions.getInt("dev");
         } else {
-            return everestVersions.getInt("core");
+            return everestVersions.getInt("dev");
         }
     }
 }
