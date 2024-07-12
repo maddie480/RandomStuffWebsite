ccf34f346c34be902e10913a42f3963b353c297a
Maddie <52103563+maddie480@users.noreply.github.com>
2024-07-12 21:38:47 +0200

    Add json to bin in Map Tree Viewer

--- front-vue/src/views/MapTreeViewer.vue
+++ front-vue/src/views/MapTreeViewer.vue
@@ -8,12 +8,15 @@
       what is actually inside a Celeste map bin!
     </p>
 
-    <p>Upload your map bin here:</p>
+    <p>
+      Upload your map bin here, or select a converted JSON file to turn it back
+      into a map bin:
+    </p>
 
     <label class="btn btn-default">
       <input
         type="file"
-        accept=".bin"
+        accept=".bin,.json"
         :disabled="loading"
         @change="selectFile"
       />
@@ -21,7 +24,7 @@
 
     <button
       class="btn btn-primary"
-      :disabled="!selectedFiles || loading"
+      :disabled="!selectedFiles || loading || isJSONFileSelected"
       @click="upload"
     >
       Upload
@@ -32,8 +35,25 @@
       :disabled="!selectedFiles || loading"
       @click="toJSON"
     >
-      Convert to JSON
+      Convert to {{ isJSONFileSelected ? "BIN" : "JSON" }}
     </button>
+
+    <div class="form-check">
+      <input
+        id="pretty-print"
+        v-model="prettyPrint"
+        :disabled="isJSONFileSelected"
+        class="form-check-input"
+        type="checkbox"
+      />
+      <label
+        class="form-check-label"
+        for="pretty-print"
+        title="Formats the JSON file, making the file easier to read, but increasing its size."
+      >
+        Pretty print
+      </label>
+    </div>
 
     <div v-if="error" class="error">
       <div class="warning">
@@ -112,6 +132,7 @@ const vue = {
     highlightLive: "",
     onlyShowHighlight: false,
     outOfBoundsOnly: false,
+    prettyPrint: false,
   }),
   methods: {
     selectFile: function (e) {
@@ -143,16 +164,47 @@ const vue = {
       this.loading = false;
     },
     toJSON: async function () {
-      await this.upload();
-
-      if (this.mapContents !== null) {
-        download(
-          JSON.stringify(this.mapContents),
-          "map.json",
-          "application/json",
-        );
+      try {
+        this.loading = true;
+        this.error = false;
         this.mapContents = null;
+        this.highlight = "";
+        this.highlightLive = "";
+
+        const endpoint = this.isJSONFileSelected
+          ? "json-to-bin"
+          : "bin-to-json";
+
+        const resultContents = (
+          await axios.post(
+            `${config.backendUrl}/celeste/${endpoint}`,
+            this.selectedFiles[0],
+            { responseType: this.isJSONFileSelected ? "blob" : "json" },
+          )
+        ).data;
+
+        if (this.isJSONFileSelected) {
+          download(resultContents, "map.bin", "application/octet-stream");
+        } else {
+          download(
+            JSON.stringify(resultContents, undefined, this.prettyPrint ? 2 : 0),
+            "map.json",
+            "application/json",
+          );
+        }
+      } catch {
+        this.error = true;
       }
+
+      this.loading = false;
+    },
+  },
+  computed: {
+    isJSONFileSelected() {
+      return (
+        this.selectedFiles &&
+        this.selectedFiles[0].name.toLowerCase().endsWith(".json")
+      );
     },
   },
 };
@@ -174,6 +226,12 @@ h1 {
   font-size: 18pt;
   color: #ff8000;
   margin: 20px;
+}
+
+.form-check {
+  margin: 10px;
+  user-select: none;
+  display: inline-block;
 }
 
 input.search {
