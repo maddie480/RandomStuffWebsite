3743802d0bee7c27aba93d33e8b63394548afcec
Maddie <52103563+maddie480@users.noreply.github.com>
2024-07-23 12:50:00 +0200

    Try switching away from host network

--- src/main/java/ovh/maddie480/randomstuff/frontend/CelesteFileSearchService.java
+++ src/main/java/ovh/maddie480/randomstuff/frontend/CelesteFileSearchService.java
@@ -65,7 +65,7 @@ public class CelesteFileSearchService extends HttpServlet {
                 message.put("exact", "true".equals(exact));
 
                 try (Socket socket = new Socket()) {
-                    socket.connect(new InetSocketAddress("127.0.1.1", 44480));
+                    socket.connect(new InetSocketAddress("backend", 44480));
                     try (OutputStream os = socket.getOutputStream();
                          OutputStreamWriter bw = new OutputStreamWriter(os, StandardCharsets.UTF_8)) {
 
