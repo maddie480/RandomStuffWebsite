33c2d210f4037abda9faa5eef5dfe76703092449
Maddie <52103563+maddie480@users.noreply.github.com>
2024-11-26 15:54:53 +0100

    Add Vencord QuickCSS on server

--- src/main/java/ovh/maddie480/randomstuff/frontend/ExtractDiscordThemeCSS.java
+++ src/main/java/ovh/maddie480/randomstuff/frontend/ExtractDiscordThemeCSS.java
@@ -9,9 +9,13 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Logger;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ExtractDiscordThemeCSS {
     private static final Logger logger = Logger.getLogger("ExtractDiscordThemeCSS");
@@ -37,36 +41,63 @@ public class ExtractDiscordThemeCSS {
             if (cssUrls.isEmpty()) throw new IOException("No CSS URL found!");
         }
 
+        Set<String> quickCssClasses;
+        try (Stream<String> lines = Files.lines(Paths.get("../../../../src/main/webapp/WEB-INF/classes/resources/static/css/vencord-quick-css.css"))) {
+            String linesFiltered = lines
+                    .filter(s -> !s.contains("https://"))
+                    .collect(Collectors.joining("\n"));
+
+            quickCssClasses = findCssClasses(linesFiltered)
+                    .stream().filter(c -> !c.matches("\\.[0-9]+"))
+                    .collect(Collectors.toSet());
+        }
+
+        Set<String> existingCssClasses = new HashSet<>(findCssClasses(IOUtils.toString(ConnectionUtils.openStreamWithTimeout("https://raw.githubusercontent.com/maddie480/Vendroid/refs/heads/main/dist/browser.css"), StandardCharsets.UTF_8)));
+
         Files.createDirectories(Paths.get("resources/static/css/discord-nitro-themes"));
         try (BufferedWriter bw = Files.newBufferedWriter(Paths.get("resources/static/css/discord-nitro-themes/common.css"), StandardCharsets.UTF_8)) {
             int i = 0;
             for (String cssUrl : cssUrls) {
                 logger.info("Fetching " + cssUrl + " (" + (++i) + "/" + cssUrls.size() + ")");
                 String css = IOUtils.toString(ConnectionUtils.openStreamWithTimeout(cssUrl), StandardCharsets.UTF_8);
+                existingCssClasses.addAll(findCssClasses(css));
+                copyCustomThemeBackgroundCss(css, bw);
+            }
+        }
 
-                while (css.contains("{")) {
-                    String classes = css.substring(0, css.indexOf("{"));
-                    css = css.substring(classes.length());
-                    StringBuilder contents = new StringBuilder();
+        quickCssClasses.removeAll(existingCssClasses);
+        if (!quickCssClasses.isEmpty()) throw new IOException("Missing classes in QuickCSS: " + quickCssClasses);
+    }
 
-                    int offset = 0;
-                    int depth = 0;
-                    do {
-                        char c = css.charAt(offset);
-                        contents.append(c);
-                        if (c == '{') depth++;
-                        if (c == '}') depth--;
-                        offset++;
-                    } while (depth > 0);
+    private static Set<String> findCssClasses(String css) {
+        return Pattern.compile("\\.[0-9a-zA-Z_-]+").matcher(css)
+                .results()
+                .map(MatchResult::group)
+                .collect(Collectors.toSet());
+    }
 
-                    css = css.substring(offset);
+    private static void copyCustomThemeBackgroundCss(String css, BufferedWriter out) throws IOException {
+        while (css.contains("{")) {
+            String classes = css.substring(0, css.indexOf("{"));
+            css = css.substring(classes.length());
+            StringBuilder contents = new StringBuilder();
 
-                    if (classes.contains(".custom-theme-background")) {
-                        classes = classes.replace(".custom-theme-background", "");
-                        if (classes.isBlank()) classes = ":root";
-                        bw.write(classes + contents);
-                    }
-                }
+            int offset = 0;
+            int depth = 0;
+            do {
+                char c = css.charAt(offset);
+                contents.append(c);
+                if (c == '{') depth++;
+                if (c == '}') depth--;
+                offset++;
+            } while (depth > 0);
+
+            css = css.substring(offset);
+
+            if (classes.contains(".custom-theme-background")) {
+                classes = classes.replace(".custom-theme-background", "");
+                if (classes.isBlank()) classes = ":root";
+                out.write(classes + contents);
             }
         }
     }
