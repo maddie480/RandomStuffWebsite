c3822d198c14ce22852cc5463ef5192b0ca7cac9
Maddie <52103563+maddie480@users.noreply.github.com>
2025-07-16 23:08:42 +0200

    Change identity of committer on EverestAPI.github.io

--- src/main/java/ovh/maddie480/randomstuff/frontend/discord/newspublisher/InteractionManager.java
+++ src/main/java/ovh/maddie480/randomstuff/frontend/discord/newspublisher/InteractionManager.java
@@ -34,7 +34,7 @@ import java.util.*;
 public class InteractionManager extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(InteractionManager.class);
 
-    private static final NewsAuthor NEWS_COMMITTER = new NewsAuthor(52103563, "maddie480");
+    private static final NewsAuthor NEWS_COMMITTER = new NewsAuthor(212421949, "maddie480-bot");
     private static final Map<Long, NewsAuthor> NEWS_AUTHORS = ImmutableMap.of(
             354341658352943115L, NEWS_COMMITTER,
             633114231935336489L, new NewsAuthor(67283043, "campbell-godfrey"),
